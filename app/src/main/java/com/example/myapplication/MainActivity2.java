c6ee1aa426576a535aff8edd4d74b74c46e9df93
Bilal Arif <bilalarif093@gmail.com>
2023-04-01 12:48:58 +0500

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    EditText num;
+    TextView res;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        initComponents();
+        settingUpListerners();
+    }
+
+    private void settingUpListerners() {
+        btn.setOnClickListener(view->{
+            int binary = Integer.parseInt(num.getText().toString());
+            int decimal=0;int i=0;
+            while (binary!=0){
+                int reminder = binary%10;
+                binary /=10;
+                decimal += reminder*Math.pow(2,i);
+                ++i;
+            }
+            res.setText("Decimal = "+decimal);
+        });
+    }
+
+    private void initComponents() {
+        num = findViewById(R.id.myNum);
+        btn = findViewById(R.id.btnC);
+        res = findViewById(R.id.res);
+    }
+}
